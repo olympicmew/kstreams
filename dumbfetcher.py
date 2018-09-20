2c8996ada70c5ba620c057f2e57cef2b42f8e743
olympicmew <olympicmew@gmail.com>
2018-09-20 04:16:55 +0200

    Add dumbfetcher.py
    This is a simple and ugly script that uses kstreams to fetch data from
    Genie. It can be called with one of three parameters (init, update and
    fetch) that correspond to the function and methods with similar names in
    the package.

---
+++ dumbfetcher.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+An ugly script that will do the job of fetching data until I'll come
+around writing something better
+"""
+
+if __name__ == '__main__':
+    import kstreams
+    from sys import argv
+
+    if argv[1] == 'init':
+        kstreams.init_db('db')
+    elif argv[1] == 'update':
+        db = kstreams.SongDB('db')
+        db.update()
+        db.save()
+    elif argv[1] == 'fetch':
+        db = kstreams.SongDB('db')
+        db.fetch()
+        db.save()
